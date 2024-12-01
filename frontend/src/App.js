a3e06320346ff91789ee097f453bf5e670625073
Arselene <arselene.dev@gmail.com>
2024-12-01 16:56:43 +0100

    init repo

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+//import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+//const Home = () => <h1>Home Page</h1>;
+//const About = () => <h1>About Page</h1>;
+
+const App = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Tailwind CSS is Working!
+      </h1>
+    </div>
+  );
+};
+
+export default App;
